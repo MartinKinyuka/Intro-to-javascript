0d317a1c3d8eb2d67c6701596f9477b966cb5731
MartinKinyuka <79451364+MartinKinyuka@users.noreply.github.com>
2021-03-08 15:13:36 +0300

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,13 @@
+var person;
+console.log(person)
+
+var person;
+person = "Christine";
+console.log(person);
+
+var lion;
+lion="Mufasa";
+var kilobyte;
+kilobyte=10;
+console.log(lion);
+console.log(kilobyte);
