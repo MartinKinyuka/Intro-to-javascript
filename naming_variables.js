3aee7431bf24661d4caf5e076afe743b271d8432
MartinKinyuka <79451364+MartinKinyuka@users.noreply.github.com>
2021-03-08 15:27:53 +0300

    Add files via upload

---
+++ naming_variables.js
@@ -0,0 +1,9 @@
+var bookNumber;
+bookNumber=3;
+console.log(bookNumber);
+var giraffe;
+giraffe="Very Tall";
+console.log(giraffe);
+var tembeaAfrica;
+tembeaAfrica="Kenya";
+console.log(tembeaAfrica);
